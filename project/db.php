177e270c9a6b326309f8e46d30a4d3e789efaf20
Hazao <vrostik750@mail.ru>
2026-01-13 11:59:36 +0300

    Delete project directory

--- project/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'notebook';
-$mysqli = mysqli_connect($host, $user, $password, $database);
-if(mysqli_connect_errno()) echo mysqli_connect_error();
-?>
